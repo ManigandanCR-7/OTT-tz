2e2612ee632e072a0ff830c3d1a4ef0ef68124bd
ManigandanCR-7 <aaron.7arun@gmail.com>
2024-12-28 14:18:12 +0530

    Add files via upload

---
+++ public/service-worker.js
@@ -0,0 +1,46 @@
+const CACHE_NAME = 'pwa-cache-v1';
+const urlsToCache = [
+  '/',
+  'index.html',
+  'app.js',
+  'manifest.json',
+  'offline.html',
+  'images/icon-192x192.png',
+  'images/icon-512x512.png'
+];
+
+// Install
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Activate
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (!cacheWhitelist.includes(cache)) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Fetch
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request).catch(() => {
+        return caches.match('/offline.html');
+      });
+    })
+  );
+});
